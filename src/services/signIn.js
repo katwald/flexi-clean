833415680b1f7e6047aa9937b7c4e3560caba815
Devendra Katwal <katwaldevendra1@gmail.com>
2024-05-23 17:59:59 +0300

    change url

--- src/services/signIn.js
+++ src/services/signIn.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const signUpURL = "http://localhost:3001/api/users";
-const signInURL = "http://localhost:3001/api/login";
+// const signUpURL = "http://localhost:3001/api/users";
+// const signInURL = "http://localhost:3001/api/login";
+const signUpURL = "/api/users";
+const signInURL = "/api/login";
 
 const signup = async (credentials) => {
   const response = await axios.post(signUpURL, credentials);
